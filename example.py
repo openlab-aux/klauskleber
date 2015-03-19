c3dcc14563aed49ec6ceef38c6e3cedccdd4945e
Michael Wendland <accounts@michiwend.com>
2015-03-19 17:58:08 +0100

    added example code

---
+++ example.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+
+from klauskleber import LabelPrinter, Label
+
+
+if __name__ == "__main__":
+
+    lp = LabelPrinter("/dev/ttyUSB1")
+
+    label = Label(
+            thing_id = "1337",
+            thing_name = "Stuff",
+            thing_maintainer = "Heinrich")
+
+    lp.print_label(label)
